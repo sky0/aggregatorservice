a61a57f2ad22f8f96377c8cd230827d728d2cd96
Vinod Jat <vinod.jat@nagarro.com>
2020-06-26 17:41:53 +0530

    Add docker file

--- src/main/java/com/nagarro/micro/aggregator/AggregatorApplication.java
+++ src/main/java/com/nagarro/micro/aggregator/AggregatorApplication.java
@@ -1,5 +1,7 @@
 package com.nagarro.micro.aggregator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -8,8 +10,13 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class AggregatorApplication {
+	
+	
+
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(AggregatorApplication.class);
+		logger.info("AggregatorApplication start");
 		SpringApplication.run(AggregatorApplication.class, args);
 	}
 
